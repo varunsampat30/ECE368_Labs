cb28237af39768b51bcd86463681b652ea0f769f
Varun Sampat <varunsampat@Varuns-laptop.local>
2020-02-25 00:41:54 -0500

    Added code for the email spam classifier and also the LDA/QDA model for height & weight data

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -3,6 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import util
 
+import math
+from decimal import Decimal
+
+def estimator(file_list, dictionary_email):
+    """
+    Returns a parameters p_d, and q_d from the training set for a given
+    file list
+    
+    Input
+    -----
+    Output
+    ------
+    
+    """
+    freq_dic = util.get_word_freq(file_list)
+    # must account for words that we encounter at least once
+    ### Laplace smoothing -> one occurrence of each word has already occured
+    ### this implies => pd = (# occ of word+1)/(Total # of words in the bag+D),
+    ### where D is the total number of words
+    
+    totalWordsinFiles = 0
+    for word in freq_dic:
+         # safety check to see if word occurs more than
+         # once
+        totalWordsinFiles += freq_dic[word]
+    estimator_list = util.Counter()
+    for word in dictionary_email:
+        estimator_list[word] = ((freq_dic[word] + 1))/((totalWordsinFiles + 
+                 len(dictionary_email)))
+    ### doesn't violate the sum of prob. because 
+    ### before any occurrences, each word in the dictionary has a uniform
+    ### prob. of 1/D, where D is the total number of words. Sigma(1/D) over
+    ### all D will be 1.
+    return estimator_list
+    
+    
 def learn_distributions(file_lists_by_category):
     """
     Estimate the parameters p_d, and q_d from the training set
@@ -20,11 +56,50 @@ def learn_distributions(file_lists_by_category):
     smoothed estimates of q_d 
     """
     ### TODO: Write your code here
+    probabilities_by_category = []
+    ## make dictionary
     
+    spam_words = []
+    for file in file_lists_by_category[0]:
+        words = util.get_words_in_file(file)
+        spam_words.extend(words)
+        
+    ham_words = []
+    for file in file_lists_by_category[1]:
+        words = util.get_words_in_file(file)
+        ham_words.extend(words)    
+    dictionary_emails = list(set(ham_words+spam_words))
     
+    probabilities_by_category.append(estimator(file_lists_by_category[0], 
+                                               dictionary_emails))
+    probabilities_by_category.append(estimator(file_lists_by_category[1], 
+                                               dictionary_emails))
     return probabilities_by_category
 
-def classify_new_email(filename,probabilities_by_category,prior_by_category):
+def probability(freq_dic, word_probabilities, prior):
+    
+    result = 1
+    numerator = 0
+    denominator = 1
+    pd_prod = []
+    
+    for word in freq_dic:
+        if word in word_probabilities:
+            numerator += freq_dic[word]
+            val = Decimal(word_probabilities[word]**freq_dic[word])
+            pd_prod.append(val)
+            denominator = denominator * math.factorial(freq_dic[word])
+    result = Decimal(prior) * (Decimal(math.factorial(numerator))/denominator)*np.prod(pd_prod)
+    return result
+    
+    """
+    result = np.log(prior)
+    for word in freq_dic:
+        result += freq_dic[word] * np.log(word_probabilities[word])
+    return result
+    """
+    
+def classify_new_email(filename, probabilities_by_category, prior_by_category, zeta):
     """
     Use Naive Bayes classification to classify the email in the given file.
 
@@ -34,6 +109,7 @@ def classify_new_email(filename,probabilities_by_category,prior_by_category):
     probabilities_by_category: output of function learn_distributions
     prior_by_category: A two-element list as [\pi, 1-\pi], where \pi is the 
     parameter in the prior class distribution
+    zeta: coefficient to change bias and influence decisions
 
     Output
     ------
@@ -43,9 +119,81 @@ def classify_new_email(filename,probabilities_by_category,prior_by_category):
     representing the log posterior probabilities
     """
     ### TODO: Write your code here
+    fileList = list()
+    fileList.append(filename)
+    wordFrequencies = util.get_word_freq(fileList)
     
+    spamProbability = probability(
+            wordFrequencies, 
+            probabilities_by_category[0], 
+            prior_by_category[0])
+    
+    hamProbability = probability(
+            wordFrequencies, 
+            probabilities_by_category[1], 
+            prior_by_category[1])
+    
+   # What is zeta? -> check code summary
+    classify_result = list()
+    if Decimal(2-zeta)*spamProbability >= Decimal(zeta)*hamProbability:
+        classify_result.append('spam')
+    else:
+        classify_result.append('ham')
+        
+    logResult = list()
+    logResult.append(np.log(float(spamProbability)))
+    logResult.append(np.log(float(hamProbability)))
+    classify_result.append(logResult)
     
     return classify_result
+
+def testModel(zeta, test_folder, probabilities_by_category, priors_by_category):
+    """
+    ----
+    Outputs: 2 element list error
+    
+    """
+    performance_measures = np.zeros([2,2])
+        # Classify emails from testing set and measure the performance
+            # explanation of performance_measures:
+    # columns and rows are indexed by 0 = 'spam' and 1 = 'ham'
+    # rows correspond to true label, columns correspond to guessed label
+    # to be more clear, performance_measures = [[p1 p2]
+    #                                           [p3 p4]]
+    # p1 = Number of emails whose true label is 'spam' and classified as 'spam' 
+    # p2 = Number of emails whose true label is 'spam' and classified as 'ham' 
+    # ^ type 1 error
+    # p3 = Number of emails whose true label is 'ham' and classified as 'spam' 
+    # ^ type 2 error
+    # p4 = Number of emails whose true label is 'ham' and classified as 'ham' 
+    for filename in (util.get_files_in_folder(test_folder)):
+        # Classify
+        label,log_posterior = classify_new_email(filename,
+                                                 probabilities_by_category,
+                                                 priors_by_category, zeta)
+    
+        # Measure performance (the filename indicates the true label)
+        base = os.path.basename(filename)
+        true_index = ('ham' in base) 
+        guessed_index = (label == 'ham')
+        performance_measures[int(true_index), int(guessed_index)] += 1
+
+    template="You correctly classified %d out of %d spam emails, and %d out of %d ham emails."
+    # Correct counts are on the diagonal
+    correct = np.diag(performance_measures)
+    # totals are obtained by summing across guessed labels
+    totals = np.sum(performance_measures, 1)
+    print(template % (correct[0],totals[0],correct[1],totals[1]))
+    return 100*(totals - correct)/totals,(performance_measures[0,1], performance_measures[1,0])
+
+def plot_error_results(zetas, error_1, error_2):
+    plt.figure()
+    plt.grid()
+    plt.plot(zetas, error_1, 'r', label='Type error 1')
+    plt.plot(zetas, error_2, 'b', label='Type error 2')
+    plt.legend()
+    plt.xlabel('Zeta')
+    plt.ylabel('Error %')
 
 if __name__ == '__main__':
     
@@ -64,42 +212,39 @@ if __name__ == '__main__':
     
     # prior class distribution
     priors_by_category = [0.5, 0.5]
-    
-    # Store the classification results
-    performance_measures = np.zeros([2,2])
-    # explanation of performance_measures:
-    # columns and rows are indexed by 0 = 'spam' and 1 = 'ham'
-    # rows correspond to true label, columns correspond to guessed label
-    # to be more clear, performance_measures = [[p1 p2]
-    #                                           [p3 p4]]
-    # p1 = Number of emails whose true label is 'spam' and classified as 'spam' 
-    # p2 = Number of emails whose true label is 'spam' and classified as 'ham' 
-    # p3 = Number of emails whose true label is 'ham' and classified as 'spam' 
-    # p4 = Number of emails whose true label is 'ham' and classified as 'ham' 
-
-    # Classify emails from testing set and measure the performance
-    for filename in (util.get_files_in_folder(test_folder)):
-        # Classify
-        label,log_posterior = classify_new_email(filename,
-                                                 probabilities_by_category,
-                                                 priors_by_category)
-        
-        # Measure performance (the filename indicates the true label)
-        base = os.path.basename(filename)
-        true_index = ('ham' in base) 
-        guessed_index = (label == 'ham')
-        performance_measures[int(true_index), int(guessed_index)] += 1
-
-    template="You correctly classified %d out of %d spam emails, and %d out of %d ham emails."
-    # Correct counts are on the diagonal
-    correct = np.diag(performance_measures)
-    # totals are obtained by summing across guessed labels
-    totals = np.sum(performance_measures, 1)
-    print(template % (correct[0],totals[0],correct[1],totals[1]))
-    
+    zeta = 1
+    error, incorrect = testModel(zeta, 
+                                 test_folder, 
+                                 probabilities_by_category, 
+                                 priors_by_category)
     
     ### TODO: Write your code here to modify the decision rule such that
     ### Type 1 and Type 2 errors can be traded off, plot the trade-off curve
-   
-
- 
+# Type 1 error is defined as the event that a spam email is misclassified as ham
+# Type 2 error is defined as the event that a ham email is misclassified as spam
+    print("Error 1: ", error[0])
+    print("Error 2: ", error[1])
+    
+    error_1_results = list()
+    error_2_results = list()
+    zetas = np.arange(0, 2.1, 0.1)
+    incorrect_1_list = []
+    incorrect_2_list = []
+    for zeta in zetas:
+        error, incorrect = testModel(zeta, test_folder, probabilities_by_category, priors_by_category)
+        error_1_results.append(error[0])
+        error_2_results.append(error[1])
+        incorrect_1_list.append(incorrect[0])
+        incorrect_2_list.append(incorrect[1])
+        
+    plot_error_results(zetas, error_1_results, error_2_results)
+    plt.figure()
+    plt.xlabel("Type 1 Errors")
+    plt.ylabel("Type 2 Errors")
+    plt.title("Type 1 vs Type 2 error")
+    plt.scatter(incorrect_1_list, incorrect_2_list)
+    plt.savefig("nbc.pdf")
+    
+        
+    
+    
--- ldaqda/ldaqda.py
+++ ldaqda/ldaqda.py
@@ -2,6 +2,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import util
 
+def meanEstimator(x):
+    """
+    - Compute the mean (MLE) of a normal variable
+    """
+    height = x[:, 0]
+    weight = x[:, 1]
+    
+    return np.sum(height)/len(height), np.sum(weight)/len(weight) 
+
+def splitData(x, y):
+    male_x = x[y == 1]
+    female_x = x[y == 2]
+    return male_x, female_x
+    
+def covariance(x, y, mu_x, mu_y):
+    result = np.sum((x-mu_x)*(y-mu_y))/(len(x))
+    return result
+
+def covarianceMatrix(x, mean_vector):
+    diff_x_mu = np.zeros((len(x), 2))
+    
+    for j in range(len(x)):
+        diff_x_mu[j] = x[j]-mean_vector
+    x_transpose = diff_x_mu.transpose()
+    cov_matrix = (1/len(x))*(np.matmul(x_transpose, diff_x_mu))
+    return cov_matrix
+
+def LDA_formula(covMatrix, male_mean, female_mean, X, Y):
+    invMatrix = np.linalg.inv(covMatrix)
+    A = np.transpose(np.matmul(invMatrix, male_mean)) - np.transpose(np.matmul(invMatrix, female_mean))
+    
+    b = 0.5*(np.matmul(np.transpose(male_mean), np.matmul(invMatrix, male_mean)) - 
+             np.matmul(np.transpose(female_mean), np.matmul(invMatrix, female_mean)))
+    return A[0]*X + A[1]*Y - b
+
+def LDA(prior, covMatrix, male_mean, female_mean):
+    # A * xT = b
+
+    x = np.arange(50, 80, 0.15)
+    y = np.arange(80, 280, 1)
+    X, Y = np.meshgrid(x, y)
+    
+    Z = LDA_formula(covMatrix, male_mean, female_mean, X, Y)
+    return X, Y, Z
+
+def QDA_formula(cov_male, cov_female, mu_male, mu_female, X, Y):
+    inv_male = np.linalg.inv(cov_male)
+    inv_female = np.linalg.inv(cov_female)
+    
+    det_male = np.linalg.det(cov_male)
+    det_female = np.linalg.det(cov_female)
+    
+    A = np.transpose(np.matmul(inv_male, mu_male)) - np.transpose(np.matmul(inv_female, mu_female))
+    b = 0.5*(np.matmul(np.transpose(mu_male), np.matmul(inv_male, mu_male)) - np.matmul(np.transpose(mu_female), np.matmul(inv_female, mu_female)))
+    return 0.5*np.log(det_male) - 0.5*np.log(det_female) + b - A[0]*X - A[1]*Y\
+       + 0.5*X*(inv_male[0, 0]*X+inv_male[0, 1]*Y) + 0.5*Y*(inv_male[1, 0]*X\
+       +inv_male[1, 1]*Y) - 0.5*X*(inv_female[0, 0]*X+inv_female[0, 1]*Y)\
+       - 0.5*Y*(inv_female[1, 0]*X+inv_female[1,1]*Y) 
+
+def QDA(prior, cov_male, cov_female, mu_male, mu_female):
+    
+    x = np.arange(50, 80, 0.15)
+    y = np.arange(80, 280, 1)
+    X, Y = np.meshgrid(x, y) 
+    
+    Z = QDA_formula(cov_male, cov_female, mu_male, mu_female, X, Y)
+    return X, Y, Z
+
+
+def dataScatterPlot(male_x, female_x):    
+    plt.figure()
+    plt.xlabel("Height")
+    plt.ylabel("Weight")
+    plt.title("Training data with the LDA boundary curve")
+    plt.scatter(male_x[:, 0], male_x[:, 1], color='blue', label='Males')
+    plt.scatter(female_x[:, 0], female_x[:, 1], color='red', label='Females')
+    
+def plotDecisionBoundary(height, weight, result):
+    plt.contour(height, weight, result, 0)
+
+def gaussianContour(height, weight, density_male, density_female):
+    plt.contour(height, weight, density_male)
+    plt.contour(height, weight, density_female)
+    plt.legend(loc='upper left')
+    
 def discrimAnalysis(x, y):
     """
     Estimate the parameters in LDA/QDA and visualize the LDA/QDA models
@@ -9,7 +94,6 @@ def discrimAnalysis(x, y):
     Inputs
     ------
     x: a N-by-2 2D array contains the height/weight data of the N samples
-    
     y: a N-by-1 1D array contains the labels of the N samples 
     
     Outputs
@@ -21,10 +105,55 @@ def discrimAnalysis(x, y):
     and 1 figure for QDA in this function         
     """
     ### TODO: Write your code here
+    male_x, female_x = splitData(x, y)
     
+    ## mean
+    mu_male = meanEstimator(male_x)
+    mu_female = meanEstimator(female_x)
+    mu = meanEstimator(x)
+    
+    ## mean values print
+    #print("male mu: ", mu_male)
+    #print("female mu: ", mu_female)
+    
+    ## covariance values
+    cov = covarianceMatrix(x, mu)
+    cov_male = covarianceMatrix(male_x, mu_male)
+    cov_female = covarianceMatrix(female_x, mu_female)
+
+    #print("Overall cov. matrix:\n", cov)    
+    #print("Male cov. matrix:\n", cov_male)
+    #print("Female cov. matrix:\n", cov_female)
+    
+    # LDA calc.
+    height, weight, result = LDA([0.5, 0.5], cov, mu_male, mu_female)
+    
+    #QDA calc.
+    height_Q, weight_Q, result_Q = QDA([0.5, 0.5], 
+                                       cov_male, 
+                                       cov_female, 
+                                       mu_male, 
+                                       mu_female)
+    
+    ## gaussian calculations
+    x_set = np.dstack((height, weight))
+    x_set = np.reshape(x_set, (len(height)*len(weight), 2))
+    density_male = util.density_Gaussian(mu_male, cov_male, x_set).reshape(height.shape[0], height.shape[1])
+    density_female = util.density_Gaussian(mu_female, cov_female, x_set).reshape(height.shape[0], height.shape[1])
+    
+    ## figure 1 (just scatter)
+    dataScatterPlot(male_x, female_x)
+    plotDecisionBoundary(height, weight, result)
+    gaussianContour(height, weight, density_male, density_female)
+    plt.savefig("lda.pdf")
+    
+    #figure 2 (QDA)
+    dataScatterPlot(male_x, female_x)
+    plotDecisionBoundary(height, weight, result_Q)
+    gaussianContour(height, weight, density_male, density_female)
+    plt.savefig("qda.pdf")
     
     return (mu_male,mu_female,cov,cov_male,cov_female)
-    
 
 def misRate(mu_male,mu_female,cov,cov_male,cov_female,x,y):
     """
@@ -43,7 +172,31 @@ def misRate(mu_male,mu_female,cov,cov_male,cov_female,x,y):
     A tuple of two elements: (mis rate in LDA, mis rate in QDA )
     """
     ### TODO: Write your code here
-   
+    
+    male_x, female_x = splitData(x, y)
+        
+
+    misHit = 0
+    for i in range(len(x)):
+        if(y[i] == 1): #supposed to be male
+            if(LDA_formula(cov, mu_male, mu_female, x[i, 0], x[i, 1]) < 0):
+                misHit += 1
+        elif(y[i] == 2): #supposed to be a female
+            if(LDA_formula(cov, mu_male, mu_female, x[i, 0], x[i, 1]) > 0):
+                misHit += 1    
+    
+    misHit_Q = 0
+    for i in range(len(x)):
+        if(y[i] == 1): #supposed to be male
+            if(QDA_formula(cov_male, cov_female, mu_male, mu_female, x[i, 0], x[i, 1]) > 0):
+                misHit_Q += 1
+        elif(y[i] == 2): #supposed to be a female
+            if(QDA_formula(cov_male, cov_female, mu_male, mu_female, x[i, 0], x[i, 1]) < 0):
+                misHit_Q += 1
+    mis_lda, mis_qda = misHit/len(x), misHit_Q/len(x)
+    print("LDA error rate: ", mis_lda)
+    print("QDA error rate: ", mis_qda)
+
     
     return (mis_lda, mis_qda)
 
