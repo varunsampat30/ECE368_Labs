60385c8f8e55273d1522b599af6e883269909f44
Varun Sampat <varunsampat@Varuns-laptop.local>
2020-04-02 15:38:09 +0530

    Initial commit for lab 3. Contains all files provided.

---
+++ ECE368_LAB_3/inference.py
@@ -0,0 +1,128 @@
+import numpy as np
+import graphics
+import rover
+
+def forward_backward(all_possible_hidden_states,
+                     all_possible_observed_states,
+                     prior_distribution,
+                     transition_model,
+                     observation_model,
+                     observations):
+    """
+    Inputs
+    ------
+    all_possible_hidden_states: a list of possible hidden states
+    all_possible_observed_states: a list of possible observed states
+    prior_distribution: a distribution over states
+
+    transition_model: a function that takes a hidden state and returns a
+        Distribution for the next state
+    observation_model: a function that takes a hidden state and returns a
+        Distribution for the observation from that hidden state
+    observations: a list of observations, one per hidden state
+        (a missing observation is encoded as None)
+
+    Output
+    ------
+    A list of marginal distributions at each time step; each distribution
+    should be encoded as a Distribution (see the Distribution class in
+    rover.py), and the i-th Distribution should correspond to time
+    step i
+    """
+
+    num_time_steps = len(observations)
+    forward_messages = [None] * num_time_steps
+    forward_messages[0] = prior_distribution
+    backward_messages = [None] * num_time_steps
+    marginals = [None] * num_time_steps 
+    
+    # TODO: Compute the forward messages
+ 
+                   
+    # TODO: Compute the backward messages
+   
+    
+    # TODO: Compute the marginals 
+            
+    return marginals
+
+def Viterbi(all_possible_hidden_states,
+            all_possible_observed_states,
+            prior_distribution,
+            transition_model,
+            observation_model,
+            observations):
+    """
+    Inputs
+    ------
+    See the list inputs for the function forward_backward() above.
+
+    Output
+    ------
+    A list of esitmated hidden states, each state is encoded as a tuple
+    (<x>, <y>, <action>)
+    """
+
+    # TODO: Write your code here
+    
+    return estimated_hidden_states
+
+
+if __name__ == '__main__':
+   
+    enable_graphics = True
+    
+    missing_observations = True
+    if missing_observations:
+        filename = 'test_missing.txt'
+    else:
+        filename = 'test.txt'
+            
+    # load data    
+    hidden_states, observations = rover.load_data(filename)
+    num_time_steps = len(hidden_states)
+
+    all_possible_hidden_states   = rover.get_all_hidden_states()
+    all_possible_observed_states = rover.get_all_observed_states()
+    prior_distribution           = rover.initial_distribution()
+    
+    print('Running forward-backward...')
+    marginals = forward_backward(all_possible_hidden_states,
+                                 all_possible_observed_states,
+                                 prior_distribution,
+                                 rover.transition_model,
+                                 rover.observation_model,
+                                 observations)
+    print('\n')
+
+
+   
+    timestep = num_time_steps - 1
+    print("Most likely parts of marginal at time %d:" % (timestep))
+    print(sorted(marginals[timestep].items(), key=lambda x: x[1], reverse=True)[:10])
+    print('\n')
+
+    print('Running Viterbi...')
+    estimated_states = Viterbi(all_possible_hidden_states,
+                               all_possible_observed_states,
+                               prior_distribution,
+                               rover.transition_model,
+                               rover.observation_model,
+                               observations)
+    print('\n')
+    
+    print("Last 10 hidden states in the MAP estimate:")
+    for time_step in range(num_time_steps - 10, num_time_steps):
+        print(estimated_states[time_step])
+  
+    # if you haven't complete the algorithms, to use the visualization tool
+    # let estimated_states = [None]*num_time_steps, marginals = [None]*num_time_steps
+    # estimated_states = [None]*num_time_steps
+    # marginals = [None]*num_time_steps
+    if enable_graphics:
+        app = graphics.playback_positions(hidden_states,
+                                          observations,
+                                          estimated_states,
+                                          marginals)
+        app.mainloop()
+        
